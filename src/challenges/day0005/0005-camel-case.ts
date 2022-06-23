18ae186bc396961e884aaf14cb6a4bf6806b0250
zoheirkhonyagar <zoheirkhonyagar@gmail.com>
2022-06-24 02:20:36 +0430

    feat: added camel case soultion part one

---
+++ src/challenges/day0005/0005-camel-case.ts
@@ -0,0 +1,81 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+let inputString: string = "";
+let inputLines: string[] = [];
+let currentLine: number = 0;
+process.stdin.on("data", function (inputStdin: string): void {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function (): void {
+  inputLines = inputString.split("\n");
+  inputString = "";
+  main();
+});
+
+function readLine(): string {
+  return inputLines[currentLine++];
+}
+
+enum CommandOperator {
+  SPLIT = "S",
+  COMBINE = "C",
+}
+
+enum CommandType {
+  VARIABLE = "V",
+  METHOD = "M",
+  CLASS = "C",
+}
+
+interface Command {
+  operator: CommandOperator;
+  type: CommandType;
+  mainString: string;
+}
+
+const normalizeInput = (input: string): string => {
+  return input.replace(/\s+$/g, "");
+};
+
+const getInput = (): string[] => {
+  const input: string[] = inputLines.map(normalizeInput);
+
+  return input;
+};
+
+const extractCommandFromStandardCommand = (
+  standardCommand: string
+): Command => {
+  const [operator, type, mainString] = standardCommand.split(";");
+
+  return {
+    operator: operator as CommandOperator,
+    type: type as CommandType,
+    mainString,
+  };
+};
+
+const getCommands = (standardCommands: string[]): Command[] => {
+  return standardCommands.map(extractCommandFromStandardCommand);
+};
+
+function main() {
+  // Enter your code here
+  const input: string[] = getInput();
+
+  const commands: Command[] = getCommands(input);
+
+  // example
+
+  const testCommands = [
+    "S;V;iPad",
+    "C;M;mouse pad",
+    "C;C;code swarm",
+    "S;C;OrangeHighlighter",
+  ];
+
+  console.log(input);
+}
